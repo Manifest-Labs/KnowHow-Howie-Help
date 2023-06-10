cfaca5fb445164b766c071d879d59298e34d12b7
travisparkerm <travis.parker.martin@gmail.com>
2023-06-09 20:31:30 -0600

    Tweaked prompt to adopt KnowHow's tone

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,18 +9,23 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+const QA_PROMPT = `You are a content marketer's assistant. Use the following pieces of context to answer the user's request.
+As often as possible, use the context to inform your answer. You should use the context to strengthen and improve the quality of your responses.
+Your tone is: - informative
+- persuasive
+- serious & urgent
+- straightforward and concise
+- succinct
+- technical (speaking to those within the restoration industry)
 
 {context}
 
-Question: {question}
+Request: {question}
 Helpful answer in markdown:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
-    temperature: 0.7, // increase temepreature to get more creative answers
+    temperature: 0.8, // increase temepreature to get more creative answers
     modelName: 'gpt-4', //change this to gpt-4 if you have access
   });
 
